1df43ec7b1de6635af60d601f6f81e582c8ae554
M. Sezgin Ruhi <sezginruhi@hotmail.com>
2023-07-25 17:46:03 +0200

    Initial Commit

---
+++ src/main/java/com/msruhi/openscoresheets/OpenScoreSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.msruhi.openscoresheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenScoreSheetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenScoreSheetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msruhi/openscoresheets/OpenScoreSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msruhi.openscoresheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenScoreSheetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
